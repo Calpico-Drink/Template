52e66be3637f7a1332cd6e62ba7ef82bf9a219de
calpico <calpicodrink@protonmail.com>
2021-04-01 16:20:51 -0700

    add next-pwa

--- src/components/Global/Footer.tsx
+++ src/components/Global/Footer.tsx
@@ -1,9 +1,10 @@
 import Styles from '@styles/Global.Footer.module.css'
+import Link from 'next/link'
 
 export default function Footer() {
     return (
         <footer className={Styles.Footer}>
-            <p>Made by Calpico</p>
+            <p>Made by <Link href="https://calpico.dev/">Calpico</Link></p>
         </footer>
     )
 }
