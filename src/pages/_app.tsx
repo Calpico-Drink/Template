4f764b6416184d3b8ee66b9a7a8447a994bb29c5
calpico <calpicodrink@protonmail.com>
2021-04-01 22:49:04 -0700

    add coloring dag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,6 +13,7 @@ function MyApp({ Component, pageProps }) {
         <title>{MetaData.Title}</title>
         <link rel="manifest" href="/manifest.json" />
     	<link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+    	<meta name="theme-color" content="#ffffff"/>
       </Head>
       <Header />
       <main className={Main.Main}>
