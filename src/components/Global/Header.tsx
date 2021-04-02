0430c4340dfcd6423d25a422764b03d32a05bcf5
calpico <calpicodrink@protonmail.com>
2021-04-01 21:16:57 -0700

    add header link

--- src/components/Global/Header.tsx
+++ src/components/Global/Header.tsx
@@ -1,10 +1,11 @@
 import Style from '@styles/Global.Header.module.css'
 import { MetaData } from '@src/config'
+import Link from 'next/link'
 
 export default function Header() {
     return (
         <header className={Style.Header}>
-            <h1>{MetaData.Title}</h1>
+            <h1><Link href="/">{MetaData.Title}</Link></h1>
         </header>
     )
 }
